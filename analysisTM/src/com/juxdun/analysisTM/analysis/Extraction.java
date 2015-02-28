6cf5b089dd6713704ffb30e1f59a9374f741d217
Juxdun <1018633103@qq.com>
2015-02-28 21:27:51 +0800

    添加中文分词的小部分

--- analysisTM/src/com/juxdun/analysisTM/analysis/Extraction.java
+++ analysisTM/src/com/juxdun/analysisTM/analysis/Extraction.java
@@ -1,7 +1,12 @@
 package com.juxdun.analysisTM.analysis;
 
+import java.io.IOException;
+import java.io.StringReader;
 import java.util.Iterator;
 import java.util.List;
+
+import org.wltea.analyzer.core.IKSegmenter;
+import org.wltea.analyzer.core.Lexeme;
 
 public class Extraction {
 
@@ -45,16 +50,38 @@ public class Extraction {
 
 	/**
 	 * 移除水军评论
+	 * @throws IOException 
 	 */
-	public void removeWaterArmyComment() {
+	public void removeWaterArmyComment(){
 		if (list.size()>0 && list.get(0) instanceof Comment) {// 如果list不空，并且里面是Comment对象
-			
-			for (Iterator iterator = list.iterator(); iterator.hasNext();) {
-				Comment comment = (Comment) iterator.next();
-				if (comment.getContent().length() < 6) {// 如果评论内容少于6个字符，认为是水军
-					iterator.remove();
+
+
+			try {
+
+				for (Iterator iterator = list.iterator(); iterator.hasNext();) {
+					Comment comment = (Comment) iterator.next();
+					if (comment.getContent().length() < 6) {// 如果评论内容少于6个字符，认为是水军
+						iterator.remove();
+						continue;
+					}else {
+						String content = comment.getContent();
+						StringReader sr = new StringReader(content);
+
+						//使用独立于Lucene的分词器。
+						IKSegmenter ik = new IKSegmenter(sr, true);
+						Lexeme lexeme = null;
+						while (null!=(lexeme=ik.next())) {
+							//logging
+						}
+					}
 				}
+				
+				
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
+
 		}
 	}
 
